03f248c2ac4e6e8466ebacdfeacfafde6099c2ac
Alicia Fortes Machado <aliciafortesmachado@gmail.com>
2022-03-08 15:56:59 +0100

    Adding function to save results as submission file.

--- src/util/utils.py
+++ src/util/utils.py
@@ -1,6 +1,10 @@
+"Util functions."
+
 import numpy as np
 from tqdm import tqdm
 from sklearn.decomposition import PCA
+import pandas as pd
+
 
 def accuracy(y, pred):
     return np.sum(y == pred)/len(y)
@@ -11,7 +15,6 @@ def images_to_hist(images, bins,low,up):
         histograms.append(np.histogram(images, bins = bins, range = (low, up), density = True))
     return np.array([histograms])
 
-
 def images_to_pca(train, n_components):
     pca = PCA(n_components)
     pca.fit(X=train)
@@ -20,3 +23,26 @@ def images_to_pca(train, n_components):
 
 def undersample(train):
     return train[:,::2]
+
+def read_data(data_path="data"):
+    """
+    Read the data from the data_path.
+    """
+    Xtr = np.array(pd.read_csv(data_path + 'Xtr.csv',header=None,sep=',',usecols=range(3072)))
+    Xte = np.array(pd.read_csv(data_path + 'Xte.csv',header=None,sep=',',usecols=range(3072)))
+    Ytr = np.array(pd.read_csv(data_path + 'Ytr.csv',sep=',',usecols=[1])).squeeze()
+
+    return Xtr, Ytr, Xte
+
+
+def save_results(Yte, results_name="Yte_pred.csv", results_path="data"):
+    """
+    Read the data from the data_path.
+    """
+
+    Yte = {'Prediction' : Yte}
+    dataframe = pd.DataFrame(Yte)
+    dataframe.index += 1
+    dataframe.to_csv('Yte_pred.csv',index_label='Id')
+
+    print("Results ready for submission!")
